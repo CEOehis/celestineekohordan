015d8bd2496212dcf82e26df650b611ec53f709b
ceoehis <ceo.ehis@outlook.com>
2018-05-06 05:10:43 +0100

    show scroll up arrow when page is scrolled down

--- src/App.js
+++ src/App.js
@@ -7,7 +7,37 @@ import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 
+import scrollTo from './utils/animatedScroll';
+
+function scrollUp() {
+  scrollTo(0, 1250);
+}
+
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      shouldShowScrolllTopArrow: false
+    }
+
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', () => {
+      if (document.documentElement.scrollTop > 100) {
+        this.setState({
+          shouldShowScrolllTopArrow: true
+        })
+      }
+      else {
+        this.setState({
+          shouldShowScrolllTopArrow: false
+        })
+      }
+    })
+  }
+
   render() {
     return (
       <div>
@@ -17,6 +47,7 @@ class App extends Component {
         <Portfolio />
         <Contact />
         <Footer />
+        {this.state.shouldShowScrolllTopArrow && <i onClick={scrollUp} className="fa fa-angle-double-up animated bounce"></i>}
       </div>
     );
   }
