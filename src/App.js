5c1b46d254d7893276e886a904934be495698a86
ceoehis <ceo.ehis@outlook.com>
2018-05-06 01:15:43 +0100

    add jumbotron

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
+import Jumbotron from './components/Jumbotron';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Navbar />
+        <Jumbotron />
       </div>
     );
   }
---
+++ src/components/Jumbotron.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Jumbotron.css';
+
+function Jumbotron() {
+  return (
+    <main className="layout-grid">
+      <div className="jumbotron">
+        <div>
+          <h1 className="intro">Hello, I'm Celestine!</h1>
+          <p>Javascript developer. FOSS enthusiast</p>
+          <div className="social-icons">
+            <i className="fa fa-github"></i>
+            <i className="fa fa-free-code-camp"></i>
+            <i className="fa fa-stack-overflow"></i>
+            <i className="fa fa-linkedin"></i>
+          </div>
+        </div>
+      </div>
+    </main>
+  )
+}
+
+export default Jumbotron;
