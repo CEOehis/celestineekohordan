4885f3531dbb64e74cb0f8640838ba9e978d3d3d
ceoehis <ceo.ehis@outlook.com>
2018-05-06 03:58:25 +0100

    add animation to scroll down arrow

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -1,12 +1,47 @@
 import React from 'react';
 import './Jumbotron.css';
 
+const
+  scrollTo = function (to, duration) {
+    const
+      element = document.scrollingElement || document.documentElement,
+      start = element.scrollTop,
+      change = to - start,
+      startDate = +new Date(),
+      // t = current time
+      // b = start value
+      // c = change in value
+      // d = duration
+      easeInOutQuad = function (t, b, c, d) {
+        t /= d / 2;
+        if (t < 1) return c / 2 * t * t + b;
+        t--;
+        return -c / 2 * (t * (t - 2) - 1) + b;
+      },
+      animateScroll = function () {
+        const currentDate = +new Date();
+        const currentTime = currentDate - startDate;
+        element.scrollTop = parseInt(easeInOutQuad(currentTime, start, change, duration));
+        if (currentTime < duration) {
+          requestAnimationFrame(animateScroll);
+        }
+        else {
+          element.scrollTop = to;
+        }
+      };
+    animateScroll();
+  };
+
+function scrollDown() {
+  scrollTo(window.innerHeight, 1250);
+}
+
 function Jumbotron() {
   return (
     <main className="layout-grid">
       <div className="jumbotron">
         <div>
-          <h1 className="intro animated fadeInDown">Hello, I'm Celestine!</h1>
+          <h1 className="intro animated fadeInDown">Hello! I'm Celestine.</h1>
           <p className="animated fadeIn">Javascript developer. FOSS enthusiast</p>
           <div className="social-icons animated slideInLeft">
             <i className="fa fa-github"></i>
@@ -15,7 +50,7 @@ function Jumbotron() {
             <i className="fa fa-linkedin"></i>
           </div>
         </div>
-        <i className="fa fa-angle-double-down animated bounce infinite"></i>
+        <i onClick={scrollDown} className="fa fa-angle-double-down animated bounce infinite"></i>
       </div>
     </main>
   )
